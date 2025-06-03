990841685da8a71a4fdbc621c759409cc2de431b
CaigaoZhang <caigaozhang@qq.com>
2025-06-03 11:52:28 +0800

    添加Test01.java

---
+++ src/main/java/com/zcg/springboot_mybatis_plus/Test01.java
@@ -0,0 +1,18 @@
+package com.zcg.springboot_mybatis_plus;
+
+/**
+ * PROJECT_NAME: springboot-mybatis-plus
+ * ClassName: Test01
+ * Package: com.zcg.springboot_mybatis_plus
+ * Description:
+ *
+ * @Author CaigaoZhang
+ * @Flag Effort, Struggle !
+ * @Create 2025-06-03 11:29:32 星期二
+ * @Version 1.0
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("Test01...");
+    }
+}
